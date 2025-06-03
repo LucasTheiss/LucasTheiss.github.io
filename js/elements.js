95d5a262529fff418e29f38bd125ab7342b4b526
LucasTheiss <lucas555andriani@gmail.com>
2025-06-03 19:38:01 -0300

    video no lugar de depoimento, swiper de cursos, ajustes em css, retirada das partes de quem somos e paciente modelo da pagina inicial

---
+++ js/elements.js
@@ -0,0 +1,72 @@
+function carregarHeader() {
+    const headerHTML = `
+        <header class="header">
+            <div>
+                <a href="index.html">
+                    <div class="logo" id="div-logo">
+                        <img src="../assets/logo.png">
+                    </div>
+                </a>
+            </div>
+            <div id="menu">
+                <ul>
+                    <li><a href="#scroll-cursos" onclick="scrollIntoView()">Nossos cursos</a></li>
+                    <li><a href="quemSomos.html" target="_blank">Quem somos</a></li>  
+                    <li><a href="#diferenciais" onclick="scrollIntoView()">Diferenciais</a></li>  
+                    <li><a href="pacienteModelo.html" target="_blank">Paciente modelo</a></li>
+                </ul>
+                <div class="mobile-menu" id="mobile-menu">
+                    <div class="menu-icon" id="menu-icon" onclick="mobileMenuToggle()">
+                        <div class="block"></div>
+                        <div class="block"></div>
+                        <div class="block"></div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <ul class="mobile-menu-list" id="mobile-menu-list">
+            <li class="mobile-menu-item"><a href="#scroll-cursos" onclick="scrollIntoView()">Nossos cursos</a></li>
+            <li class="mobile-menu-item"><a href="quemSomos.html" target="_blank">Quem somos</a></li>  
+            <li class="mobile-menu-item"><a href="#diferenciais" onclick="scrollIntoView()">Diferenciais</a></li>  
+            <li class="mobile-menu-item"><a href="pacienteModelo.html" target="_blank">Paciente modelo</a></li>
+            <li class="mobile-menu-item"><a href="" onclick="scrollIntoView()">Área do aluno</a></li>
+        </ul>
+    `
+
+    const element = document.querySelector('main') || document.body
+    element .insertAdjacentHTML('afterbegin', headerHTML)
+}
+carregarHeader()
+
+function carregarFooter() {
+    const footerHTML = `
+        <footer>
+            <center id="footer-titulo">
+                <div id="suporte">
+                    Contate o suporte
+                    <form>
+                        <input id="email" name="email" placeholder="E-mail" type="email">
+                        <input id="nome" name="nome" placeholder="Nome" type="text">
+                        <input id="mensagem" name="mensagem" placeholder="Mensagem" type="text"> 
+                        <button>Enviar</button>
+                    </form>
+                </div>
+                <hr>
+                <div class="informacoes">
+                    <div>Rua Miguel Matte, 687, Edifício Evolution Corporate<br class="mobile-no-display"><br>Bairro Pioneiros, Balneário Camboriú - SC, 88331 - 030<br><br>Telefone: +55 (47) 99616-0560<br class="mobile-no-display"><br>E-mail: contato@movemedical.com.br</div>
+                    <div>
+                        <center id="redes">
+                            <br>Redes sociais<br><a href="https://www.instagram.com/move.medical?igsh=MXJ4NXZ1cTR4bHRkZw%3D%3D&utm_source=qr" target="_blank"><img src="../assets/iconinstagram.png"></a> &nbsp&nbsp<a target="_blank" href="https://www.tiktok.com/@move.medical?_t=ZM-8tGPtzKThDy&_r=1 "><img src="../assets/icontiktok.png"></a><br>
+                        </center>
+                        <!-- <a style="display: none;" href=""><img src=""></a> -->
+                    </div>
+                    <br><div id="right-side-desktop">RESIMED LTDA<br  class="mobile-no-display"><br>CNPJ 46.976.124/0001-08<br></div><br>
+                </div>
+            </center>
+            
+        </footer>
+    `
+
+    document.body.insertAdjacentHTML('beforeend', footerHTML)
+}
+carregarFooter()
---
+++ js/index.js
@@ -0,0 +1,165 @@
+document.addEventListener('DOMContentLoaded', function () {
+    if (window.innerWidth > 958){
+        function switchBackground() {
+            var background = document.getElementById("background");
+            var divTexto = document.getElementById('div-texto');
+
+            var backgrounds = [
+                '../assets/background1.png',
+                '../assets/background2.png'
+            ]
+
+            var textos = [
+                '<center class="texto" id="texto">A excelência é conquistada <br>através da prática constante</center>',
+                '<center class="texto" id="texto">Mova-se com a <span style="color: rgb(0, 213, 255)">Move</span></center><br>',
+            ]
+
+            var currentIndex = 1;
+
+            function updateContent() {
+                background.style.opacity = 0.8;
+                divTexto.style.opacity = 0.8;
+
+                setTimeout(function() {
+                    background.style.backgroundImage = `url(${backgrounds[currentIndex]})`;
+                    divTexto.innerHTML = textos[currentIndex];
+
+                    setTimeout(function() {
+                        background.style.opacity = 1;
+                        divTexto.style.opacity = 1;
+                    }, 150);
+
+                    currentIndex = (currentIndex + 1) % backgrounds.length;
+                }, 50);
+            }
+
+            setInterval(updateContent, 7000);
+        }
+        switchBackground()
+    }
+    const estruturaswiper = new Swiper('.swiper-estrutura', {
+        direction: 'horizontal',
+        grabCursor: true,
+        // centeredSlides: true,
+        loop: true,
+        slidesPerView: 1,
+        navigation: {
+            nextEl: '#swiper-button-next',
+            prevEl: '#swiper-button-prev',
+        },
+        pagination: {
+            el: '.estrutura-pagination',
+            clickable: true,
+        },
+    });
+    const swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        grabCursor: true,
+        // centeredSlides: true,
+        loop: true,
+        slidesPerView: 3,
+        navigation: {
+            nextEl: '#swiper-button-next',
+            prevEl: '#swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+            breakpoints: {
+            0: {
+                slidesPerView: 1,
+            },
+            640: {
+                slidesPerView: 2,
+            },
+            1049: {
+                slidesPerView: 3,
+            },
+        },
+    });
+    const depoimentoswiper = new Swiper('.swiper-depoimento', {
+        direction: 'horizontal',
+        loop: true,
+        grabCursor: true,
+        centeredSlides: true,
+        
+        navigation: {
+            nextEl: '#depoimento-next',
+            prevEl: '#depoimento-prev',
+        },
+        spaceBetween: 30,
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+            },
+            640: {
+                slidesPerView: 2,
+            },
+            1049: {
+                slidesPerView: 3,
+            },
+            1388: {
+                slidesPerView: 3,
+            },
+        },
+    });
+    depoimentoswiper.on('slideChange', () => {
+  document.querySelectorAll('.video-depoimento').forEach(video => {
+    video.pause();
+    video.style.display = 'none';
+    const img = video.closest('.card-depoimento').querySelector('.imagem-depoimento');
+    if (img) img.style.display = 'block';
+  });
+});
+})
+
+window.onscroll = function () {
+    const botao = document.getElementById("voltar-topo");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    botao.style.display = "flex";
+    } else {
+    botao.style.display = "none";
+    }
+};
+
+// Função para rolar até o topo suavemente
+function voltarAoTopo() {
+    window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+    });
+}
+
+function mobileMenuToggle(){
+    let menu = document.getElementById("mobile-menu-list")
+    let menuIcon = document.getElementById("menu-icon")
+    
+    if (menuIcon.classList.contains('active')){
+        menuIcon.classList.remove("active")
+        menu.classList.remove("active")
+        return
+    }
+    menuIcon.classList.toggle("active")
+    menu.classList.toggle("active")
+}
+
+function fecharModal() {
+    var modal = document.getElementById('myModal');
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    var modal = document.getElementById('myModal');
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function diferenciais(){
+    var diferenciais = document.getElementById('image-grid')
+    for (i = 0; i < 6; i++){
+        diferenciais.innerHTML += `<img src="../assets/diferencial${i + 1}.png">`
+    }
+}
+diferenciais()
